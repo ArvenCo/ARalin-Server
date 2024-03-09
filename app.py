7e9b172de9a96aca91564176c0be406009021d59
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2024-03-09 10:16:11 +0800

    bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from libs import *
+
+from src.routes.web import web
 
 app = Flask(__name__)
+
+def main():
+    app.register_blueprint(web)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
