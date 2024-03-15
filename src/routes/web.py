e70da3e3c83619fd503315feb855fddb14c5a229
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2024-03-16 02:21:46 +0800

    login layout

--- src/routes/web.py
+++ src/routes/web.py
@@ -5,3 +5,7 @@ web = Blueprint('web', __name__)
 @web.route('/')
 def index():
     return render_template('index.html')
+
+@web.route('/login')
+def login():
+    return render_template('auth/login.html')
