3c4d57c6697eef956d3ab22e397634ae01069c99
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2024-03-29 07:24:39 +0800

    created class

--- src/controller/project.py
+++ src/controller/project.py
@@ -1,26 +1,32 @@
 from libs import *
 from src.models import Project, db
-def store():
-    title  = request.form['title']
-    description = request.form['description']
-    images = request.files.getlist('images')
-    video = request.files['video']
-    
-    project = Project(title=title, description=description)
-    db.session.add(project)
-    db.session.commit()
-    db.session.refresh(project)
-    
-    
-    image_upload_path = f"static/uploads/{project.id}/images"
-    video_upload_path = f"static/uploads/{project.id}/video"
-    print(image_upload_path)
-    if not os.path.exists(image_upload_path):
-    
-        os.makedirs(image_upload_path)
-        os.makedirs(video_upload_path)
-    for image in images:
-        print(image.filename)
-        image.save(os.path.join(image_upload_path, image.filename))
-    video.save(os.path.join(video_upload_path, video.filename))
+
+class ProjectController:
+    def index():
+        projects = Project.query.all()
+        return render_template('index.html', projects=projects)
+
+    def store():
+        title  = request.form['title']
+        description = request.form['description']
+        images = request.files.getlist('images')
+        video = request.files['video']
+        
+        project = Project(title=title, description=description)
+        db.session.add(project)
+        db.session.commit()
+        db.session.refresh(project)
+        
+        
+        image_upload_path = f"static/uploads/{project.id}/images"
+        video_upload_path = f"static/uploads/{project.id}/video"
+        print(image_upload_path)
+        if not os.path.exists(image_upload_path):
+        
+            os.makedirs(image_upload_path)
+            os.makedirs(video_upload_path)
+        for image in images:
+            print(image.filename)
+            image.save(os.path.join(image_upload_path, image.filename))
+        video.save(os.path.join(video_upload_path, video.filename))
     
