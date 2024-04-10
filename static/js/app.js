1d7c9a856ff48f99fe99c1aa0177f88877e3009e
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2024-04-10 21:12:24 +0800

    video sample play

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
 var figure = $(".video").hover( hoverVideo, hideVideo );
 
 function hoverVideo(e) { $('video', this).get(0).play(); }
-function hideVideo(e) { $('video', this).get(0).pause(); }
+function hideVideo(e) { 
+    $('video', this).get(0).currentTime = 0;
+    $('video', this).get(0).pause();
+}
